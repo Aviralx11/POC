7137d1b37e48015b8dfb17db8c4bcf4ca7a8dd30
Bansal <aviral.bansal@innovasolutions.com>
2025-09-19 14:42:32 +0530

    comments

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -15,7 +15,7 @@ const PrivateRoute = ({ children }) => {
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
-//if user aint logged in, they are sent back to login
+//if user ain't logged in, they are sent back to login
 function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
